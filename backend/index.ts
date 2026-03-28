bdb533fa095414f10db1dea1c55494cc6c4b8c81
RahulSH004 <rs739025@gmail.com>
2026-03-28 13:03:00 +0000

    add cors

--- backend/index.ts
+++ backend/index.ts
@@ -1,10 +1,17 @@
 import express from "express";
 import 'dotenv/config'
+import cors from 'cors'
 import authRouter from "./src/auth/auth_routes";
 import { errorHandler } from "./src/utils/ErrorHandler";
 const PORT = process.env.PORT;
 
 const app = express()
+
+app.use(cors({
+    origin: "http://localhost:5000",
+    credentials: true
+}))
+app.use(express.json());
 
 app.use("/api", authRouter);
 
