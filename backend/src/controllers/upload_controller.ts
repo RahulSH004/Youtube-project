d4d37b4cc8c4950331a60522caef58ba23f8c5d6
RahulSH004 <rs739025@gmail.com>
2026-04-07 23:13:50 +0000

    change schema for uploads

--- backend/src/controllers/upload_controller.ts
+++ backend/src/controllers/upload_controller.ts
@@ -1,5 +1,8 @@
+import { Request, Response } from 'express';
 
+export async function UploadController(req: Request, res: Response) {
+    const userId = req.user?.id;
+    if(!userId) return res.status(401).json({message: "Unauthorized"})
 
-export async function UploadController(){
     
 }
